2b31dce1dbd67429e4cf54f6daf3e12e2a091918
ajg7 <ajgebara@gmail.com>
2020-10-16 20:24:39 -0500

    made rough draft of migration file

--- database/migrations/20201016200324_init.js
+++ database/migrations/20201016200324_init.js
@@ -1,8 +1,48 @@
 
 exports.up = function(knex) {
-
+    return knex.schema
+        .createTable("users", table => {
+            table.increments();
+            table.string("username")
+                .notNullable()
+                .unique()
+                .index()
+            table.string("password")
+                .notNullable()
+            table.integer('article_id')
+                .unsigned()
+                .notNullable()
+                .references('id')
+                .inTable('articles')
+                .onDelete('CASCADE')
+                .onUpdate('CASCADE');
+        })
+        .createTable("articles", table => {
+            table.incremets();
+            table.string("title")
+                .notNullable()
+                .unique()
+            table.string("author")
+                .notNullable()
+            table.string("summary")
+                .unique()
+            table.string("image")
+            table.string("category")
+                .notNullable()
+            table.integer("rank")
+                .notNullable()
+            table.integer('user_id')
+                .unsigned()
+                .notNullable()
+                .references('id')
+                .inTable('users')
+                .onDelete('CASCADE')
+                .onUpdate('CASCADE');
+        })
 };
 
 exports.down = function(knex) {
-
+    return knex.schema
+        .dropTableIfExists("users")
+        .dropTableIfExists("articles");
 };
